8e46909f923294b75959d6e79d7112e40aff6b64
Ido Don <76511079+Ido-Don@users.noreply.github.com>
2023-08-13 23:06:30 +0300

    added bigger shaz capabilities to the app

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,49 @@
 <script lang="ts">
-  import { change_font } from "./lib/transitions";
-  let age = 21;
-  $: visible = age < 20;
+  let age_input;
+  function reset_age() {
+    age_input = 21;
+  }
+  reset_age();
+  $: age = age_input | 0;
+
+  const SHAZ_AGE = 20;
+  $: is_shaz = age < SHAZ_AGE;
+
+  let dialog;
+  $: if (is_shaz) {
+    dialog.showModal();
+  }
 </script>
 
 <main class="center">
   <label class="center">
     הכנס את גילך
-    <input type="range" bind:value={age} />
+    <input type="number" bind:value={age_input} min={0} />
   </label>
 
-  <p>{age}</p>
-  <div class="center">
-    {#if visible}
-      <card transition:change_font={{ duration: 300 }}>שצ</card>
-    {/if}
-  </div>
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <dialog
+    bind:this={dialog}
+    on:close={reset_age}
+    on:click|self={() => dialog.close()}
+  >
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <img src="./images/shaz2.jpg" />
+  </dialog>
 </main>
 
 <style>
+  /* Chrome, Safari, Edge, Opera */
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: inherit;
+  }
+
+  /* Firefox */
+  input[type="number"] {
+    appearance: none;
+    -moz-appearance: textfield;
+  }
   .center {
     display: flex;
     flex-direction: column;
@@ -26,5 +51,28 @@
     justify-content: space-evenly;
     min-height: 1.5rem;
     gap: 1rem;
+  }
+  dialog[open] {
+    animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+  }
+  @keyframes zoom {
+    from {
+      transform: scale(0.8);
+    }
+    to {
+      transform: scale(1);
+    }
+  }
+
+  dialog[open]::backdrop {
+    animation: fade 0.2s ease-out;
+  }
+  @keyframes fade {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
   }
 </style>
